fe7bdad60b93ac07d508f8e6d88446b0b3f89337
Boarders <callan.mcgill@gmail.com>
2026-01-30 23:08:43 -0500

    Add rest of Functor def

--- CategoryTheory/Functor.lean
+++ CategoryTheory/Functor.lean
@@ -5,7 +5,11 @@ universe u₁ u₂ v₁ v₂ u v
 namespace Cat
 
 open Quiver
+open DeductiveSystem
+open Functor
 
 structure Functor (C : Type u₁) [Category C] (D : Type u₂) [Category D] where
   F₀ : C → D
-  F₁ : ∀ {c₁ c₂ : C}, Quiver.Hom c₁ c₂ → Quiver.Hom (F₀ c₁) (F₀ c₂)
+  F₁ : ∀ {c₁ c₂ : C}, Hom c₁ c₂ → Hom (F₀ c₁) (F₀ c₂)
+  F_id : ∀ {c : C}, F₁ (id c) = (DeductiveSystem.id (F₀ c))
+  F_comp : ∀ {c : C}
