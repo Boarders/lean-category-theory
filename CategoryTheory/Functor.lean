763e55b1f1110cad7ff7c6797e6cc7f7f25a4aff
Boarders <callan.mcgill@gmail.com>
2026-01-31 13:34:35 -0500

    Finish Functor def

--- CategoryTheory/Functor.lean
+++ CategoryTheory/Functor.lean
@@ -6,10 +6,11 @@ namespace Cat
 
 open Quiver
 open DeductiveSystem
-open Functor
+open Category
 
 structure Functor (C : Type u₁) [Category C] (D : Type u₂) [Category D] where
   F₀ : C → D
   F₁ : ∀ {c₁ c₂ : C}, Hom c₁ c₂ → Hom (F₀ c₁) (F₀ c₂)
   F_id : ∀ {c : C}, F₁ (id c) = (DeductiveSystem.id (F₀ c))
-  F_comp : ∀ {c : C}
+  F_comp : ∀ {a b c : C} (f : Hom a b) (g : Hom b c),
+    F₁ (f ≫ g) = F₁ f ≫ F₁ g
