bc9a43ea3f7d92dd9966b79b791c9a293bfa41c8
Boarders <callan.mcgill@gmail.com>
2026-02-03 20:03:08 -0500

    Split functor into QuiverHom + laws

--- CategoryTheory/Functor.lean
+++ CategoryTheory/Functor.lean
@@ -8,9 +8,12 @@ open Quiver
 open DeductiveSystem
 open Category
 
-structure Functor (C : Type u₁) [Category C] (D : Type u₂) [Category D] where
-  F₀ : C → D
-  F₁ : ∀ {c₁ c₂ : C}, Hom c₁ c₂ → Hom (F₀ c₁) (F₀ c₂)
+structure QuiverHom (Q₁ : Type u₁) [Quiver.{v₁} Q₁] (Q₂ : Type u₂) [Quiver.{v₂} Q₂] where
+  F₀ : Q₁ → Q₂
+  F₁ : ∀ {q₁ q₂ : Q₁}, Hom q₁ q₂ → Hom (F₀ q₁) (F₀ q₂)
+
+structure Functor (C : Type u₁) [Category C] (D : Type u₂) [Category D]
+    extends QuiverHom C D where
   F_id : ∀ {c : C}, F₁ (id c) = (DeductiveSystem.id (F₀ c))
   F_comp : ∀ {a b c : C} (f : Hom a b) (g : Hom b c),
     F₁ (f ≫ g) = F₁ f ≫ F₁ g
