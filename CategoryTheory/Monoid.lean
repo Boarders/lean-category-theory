7127df25a29886b18df8b15bbed5a5d571aa9b11
Boarders <callan.mcgill@gmail.com>
2026-02-05 15:54:30 -0500

    polish off unfinished parts of free monoid

--- CategoryTheory/Monoid.lean
+++ CategoryTheory/Monoid.lean
@@ -44,16 +44,50 @@ instance {α : Type u} : Monoid (List α) where
   mul_one := append_nil
   mul_assoc := append_assoc
 
+open Monoid
+
 def fold {α : Type u}{M : Type v} [Monoid M] (f : α → M) (xs : List α) : M :=
   match xs with
   | [] => 1
   | cons x xs => f x * (fold f xs)
 
+def fold_homomorphic {α : Type u}{M : Type v} [Monoid M] (f : α → M) (xs ys : List α) :
+  fold f (xs * ys) = fold f xs * fold f ys := by
+  induction xs with
+  | nil =>
+     simp [fold]
+     rw [show ([] : List α) * ys = ys from one_mul ys]
+  | cons x xs IH =>
+     change fold f ((cons x xs) ++ ys) = (fold f (x ∷ xs)) * (fold f ys)
+     simp [fold, append]
+     have eq : fold f (xs ++ ys) = fold f xs * fold f ys := IH
+     rw [eq, Semigroup.mul_assoc]
 
-def fold_hom {α : Type u}{M : Type v} [Monoid M]  (f : α → M) (xs : List α) : MonoidHom (List α) M := by
+def fold_hom {α : Type u}{M : Type v} [Monoid M]  (f : α → M) : MonoidHom (List α) M := by
     refine {toFun := ?_, map_one' := ?_, map_mul' := ?_}
     · exact fold f
-    sorry
+    · simp [fold]
+    · exact fold_homomorphic f
 
-theorem fold_uniq {α : Type u}{M : Type v} [Monoid M] (xs : List α) (f : α → M) (μ : MonoidHom (List α) M) (a : α) : (eq : μ (a ∷ []) = f a) → μ xs = fold f xs := by
-  sorry
+theorem fold_uniq {α : Type u}{M : Type v} [Monoid M] (xs : List α) (f : α → M) (μ : MonoidHom (List α) M) (eq_elements : (a : α) → μ (a ∷ []) = f a) : μ xs = fold f xs := by
+  induction xs with
+  | nil =>
+      have μ_eq : μ [] = 1 := by
+        apply μ.map_one'
+      have hom_eq : fold f [] = 1 := by
+        rfl
+      rw [μ_eq, hom_eq]
+  | cons x xs IH =>
+      have eq_cons : (x ∷ xs) = (x ∷ []) * xs := by
+        rfl
+      have eq_μ : μ (x ∷ xs) = μ (x ∷ []) * μ xs := by
+        rw [eq_cons]
+        apply μ.map_mul'
+      have eq_f : fold f (x ∷ xs) = fold f (x ∷ []) * fold f xs := by
+        rw [eq_cons]
+        apply fold_homomorphic f
+      rw [eq_μ, eq_f, IH]
+      rw [eq_elements x]
+      have fold_hom_extends : fold f (x ∷ []) = f x := by
+        simp [fold]
+      rw [fold_hom_extends]
