2a37b1072d36c0d43a8d47242fb90fc6b4b1478d
Boarders <callan.mcgill@gmail.com>
2026-02-06 12:54:05 -0500

    Finish adding identity functor and composition of functors

--- CategoryTheory/Functor.lean
+++ CategoryTheory/Functor.lean
@@ -40,9 +40,27 @@ def Id_Functor (C : Type u) [Category C] : Functor C C :=
 
 /-!
 ### Composition of functors
+
+  C ---F---> D ---G---> E
+
+  - This is defined as the function composition of the actions of F and G on objects
+    and morphisms
+
+  - The functor laws follow from those of the underlying functors e.g
+    F (G id) = F (id) = id etc.
 -/
 
--- def Comp_Hom (Q₁ Q₂ Q₃ : Type u) [Quiver Q₁] [Quiver Q₂] [Quiver Q₃] (F : QuiverHom Q₁ Q₂) (G : QuiverHom Q₂ Q₃) : QuiverHom Q₁ Q₃ :=
---   { F₀ := G.F₀ ∘ F.F₀
---     F₁ := by sorry
---   }
+def Comp_Hom {Q₁ Q₂ Q₃ : Type u} [Quiver Q₁] [Quiver Q₂] [Quiver Q₃] (F : QuiverHom Q₁ Q₂) (G : QuiverHom Q₂ Q₃) : QuiverHom Q₁ Q₃ :=
+  { F₀ := G.F₀ ∘ F.F₀
+    F₁ := G.F₁ ∘ F.F₁
+  }
+
+def Comp_Functor (C D E : Type u) [Category C] [Category D] [Category E] (F : Functor C D) (G : Functor D E) : Functor C E :=
+  { Comp_Hom F.toQuiverHom G.toQuiverHom with
+    F_id := by
+      intro c
+      simp [Comp_Hom, F.F_id, G.F_id]
+    F_comp := by
+      intro c d e f g
+      simp [Comp_Hom, F.F_comp, G.F_comp]
+  }
