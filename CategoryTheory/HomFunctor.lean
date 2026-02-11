c9e5158ea9db154109b077683d7024d70232ac47
Boarders <callan.mcgill@gmail.com>
2026-02-10 20:41:52 -0500

    Begin adding hom fucntors

---
+++ CategoryTheory/HomFunctor.lean
@@ -0,0 +1,18 @@
+import CategoryTheory.Category
+import CategoryTheory.Functor
+import CategoryTheory.ContravariantFunctor
+
+universe u₁ u₂ v₁ v₂ u v
+
+namespace Cat
+open Quiver
+open DeductiveSystem
+open Category
+
+def Representable {C : Type u} [Category C] (c : C) : ContravariantFunctor C (Type u) := by
+  refine {F₀ := ?_, F₁ := ?_, F_id := ?_, F_comp := ?_}
+  · intro c'
+    exact (Hom c' c)
+  · sorry
+  · sorry
+  · sorry
