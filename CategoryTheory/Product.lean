d5624d0f41223190a01fbfd2dea01c6df6cf6a86
Boarders <callan.mcgill@gmail.com>
2026-02-25 13:02:45 -0500

    Update products to use subtypes and correct def

--- CategoryTheory/Product.lean
+++ CategoryTheory/Product.lean
@@ -1,6 +1,7 @@
 import CategoryTheory.Category
 import CategoryTheory.Commutative
 import CategoryTheory.Morphisms
+import Mathlib.Logic.ExistsUnique
 
 universe u₁ u₂ v₁ v₂ u v
 
@@ -13,10 +14,13 @@ open DeductiveSystem
        ↓
        c
 -/
-structure IsProduct {C : Type u}[Category.{v} C] {a b c : C} (proj₁ : Hom a b) (proj₂ : Hom a c) : Prop where
+structure IsProduct {C : Type u}[Category.{v} C] {a b c : C} (proj₁ : Hom a b) (proj₂ : Hom a c) where
   mediating_morphism : ∀ {a' : C}
     (proj₁' : Hom a' b) (proj₂' : Hom a' c),
-    ∃! (i : Hom a' a) , i ≫ proj₁ = proj₁' ∧ i ≫ proj₂ = proj₂'
+    {i : Hom a' a // i ≫ proj₁ = proj₁' ∧ i ≫ proj₂ = proj₂'}
+  unique : ∀ {a' : C} (proj₁' : Hom a' b) (proj₂' : Hom a' c)
+    (j : Hom a' a), j ≫ proj₁ = proj₁' → j ≫ proj₂ = proj₂' →
+    j = (mediating_morphism proj₁' proj₂').val
 
 structure ProductData {C : Type u} [Category C] (b c : C) where
   obj : C
@@ -26,7 +30,7 @@ structure ProductData {C : Type u} [Category C] (b c : C) where
 
 /--
 When we say a category has all products, we mean that there is some specific choice
-of product for each pair of objects.
+of product strcuture for each pair of objects.
 -/
 class HasProducts (C : Type u)[Category C] where
   mkProduct : ∀ (c d : C), ProductData c d
@@ -34,6 +38,9 @@ class HasProducts (C : Type u)[Category C] where
 def product_obj {C : Type u}[Category C] [hp : HasProducts C]
   (b c : C) : C :=
   (hp.mkProduct b c).obj
+
+/-- Notation for composition of morphisms in a category (diagrammatic order) -/
+infixr:60 " × " => product_obj
 
 def product_proj₁ {C : Type u}[Category C] [hp : HasProducts C]
   (b c : C) : Hom (product_obj b c) b :=
@@ -43,6 +50,41 @@ def product_proj₂ {C : Type u}[Category C] [hp : HasProducts C]
   (b c : C) : Hom (product_obj b c) c :=
   (hp.mkProduct b c).proj₂
 
+/-- Notation for projection maps -/
+notation "Pr₁" => product_proj₁
+notation "Pr₂" => product_proj₂
+
 def product_proof {C : Type u}[Category C] [hp : HasProducts C]
-  (b c : C) : IsProduct (product_proj₁ b c) (product_proj₂ b c) :=
+  (b c : C) : IsProduct (Pr₁ b c) (Pr₂ b c) :=
   (hp.mkProduct b c).is_product
+
+def fork {C : Type u}[Category C] [hp : HasProducts C] {x a b : C}
+  (f : Hom x a) (g : Hom x b) : Hom x (a × b) :=
+  ((product_proof a b).mediating_morphism f g)
+
+/-- Use algebra of programming notation ▵ for fork map to a product -/
+infix:80 " ▵ " => fork
+
+theorem fork_β  {C : Type u}[Category C] [hp : HasProducts C] {x a b : C}
+  (f : Hom x a) (g : Hom x b) : (f ▵ g) ≫ Pr₁ a b = f ∧ (f ▵ g) ≫ Pr₂ a b = g := by
+  simp [fork]
+  exact ((product_proof a b).mediating_morphism f g).property
+
+@[simp] theorem fork_β₁  {C : Type u}[Category C] [hp : HasProducts C] {x a b : C}
+  (f : Hom x a) (g : Hom x b) : (f ▵ g) ≫ Pr₁ a b = f := by
+  apply And.left
+  apply fork_β f g
+
+@[simp] theorem fork_β₂  {C : Type u}[Category C] [hp : HasProducts C] {x a b : C}
+  (f : Hom x a) (g : Hom x b) : (f ▵ g) ≫ Pr₂ a b = g := by
+  apply And.right
+  apply fork_β f g
+
+def product {C : Type u}[Category C] [hp : HasProducts C] {a b c d : C}
+  (f : Hom a c) (g : Hom b d) : Hom (a × b) (c × d) :=
+  let proj₁' := (product_proj₁ a b) ≫ f
+  let proj₂' := (product_proj₂ a b) ≫ g
+  ((product_proof c d).mediating_morphism proj₁' proj₂')
+
+/-- Use algebra of programming notation □ for product map -/
+infix:80 " □ " => product
