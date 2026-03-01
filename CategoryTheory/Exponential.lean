a2f3b7b5d6f997aeb28792f5682261c6784d407c
Boarders <callan.mcgill@gmail.com>
2026-03-01 10:43:44 -0500

    Add exponential adjoint

--- CategoryTheory/Exponential.lean
+++ CategoryTheory/Exponential.lean
@@ -19,12 +19,12 @@ open DeductiveSystem
 -/
 structure Exponential {C : Type u} [Category C] [HasProducts C] (a b : C) where
   obj : C
-  eval : Hom (obj × α) b
+  eval : Hom (obj × a) b
   adj  : ∀ {c : C} (_f : (c × a) ⇒ b), (c ⇒ obj)
-  β : ∀ {c : C} (f : (c × a) ⇒ b) , ((adj f) □ (𝟙 a)) ≫ eval = f
+  β : ∀ {c : C} (f : (c × a) ⇒ b) , (adj f) □ (𝟙 a) ≫ eval = f
   uniq :
     ∀ {c : C} (f : (c × a) ⇒ b) (adj_f' : c ⇒ obj) ,
-      (((adj_f') □ (𝟙 a)) ≫ eval = f) -> adj_f' = adj f
+      (adj_f' □ (𝟙 a) ≫ eval = f) -> adj_f' = adj f
 
 -- Any map into a function object is equivalent to a lambda
 theorem eta  {C : Type u} [Category C] [HasProducts C] {a b c : C}
@@ -34,6 +34,17 @@ theorem eta  {C : Type u} [Category C] [HasProducts C] {a b c : C}
   apply Exp.uniq ((f □ (𝟙 a)) ≫ Exp.eval) f
   rfl
 
+def exponentialAdjoint' {C : Type} [Category C] [HasProducts C] {a b c : C} (Exp : Exponential b c) : Hom (a × b) c ≃ Hom a Exp.obj := by
+  refine ⟨?_, ?_, ?_, ?_⟩
+    -- map from a × b ⇒ c → a ⇒ c^b
+  · exact Exp.adj
+    -- map from a ⇒ c^b to a × b ⇒ c
+  · intro f
+    exact (f □ (𝟙 b) ≫ Exp.eval)
+  · intro f
+    simp [Exp.β f]
+  · intro f
+    simp [eta Exp f]
 
 /--
 When we say a category has all exponentials, we mean that there is some specific choice
@@ -42,18 +53,13 @@ of exponential object and sructure maps for each pair of objects.
 class HasExponentials (C : Type u)[Category C] [HasProducts C] where
   mkExponential : ∀ (c d : C), Exponential c d
 
--- def exponential_obj {C : Type u}[Category C] [HasProducts C] [hp : HasExponentials C]
---   (b c : C) : C :=
---   (hp.mkExponential b c).obj
 
--- def exponential_proj₁ {C : Type u}[Category C]  [HasProducts C] [hp : HasExponentials C]
---   (b c : C) : Hom (exponential_obj b c) b :=
---   (hp.mkExponential b c).proj₁
 
--- def exponential_proj₂ {C : Type u}[Category C] [HasProducts C] [hp : HasExponentials C]
---   (b c : C) : Hom (exponential_obj b c) c :=
---   (hp.mkExponential b c).proj₂
+def exponential_obj {C : Type u}[Category C] [HasProducts C] [hp : HasExponentials C]
+  (b c : C) : C :=
+  (hp.mkExponential b c).obj
 
--- def exponential_proof {C : Type u}[Category C]  [HasProducts C] [hp : HasExponentials C]
---   (b c : C) : IsExponential (exponential_proj₁ b c) (exponential_proj₂ b c) :=
---   (hp.mkExponential b c).is_exponential
+infixr:60 " ~> " => exponential_obj
+
+def exponentialAdjoint {C : Type} [Category C] [HasProducts C] [Exp : HasExponentials C] {a b c : C} : Hom (a × b) c ≃ Hom a (b ~> c) := by
+  apply exponentialAdjoint' (Exp.mkExponential b c)
